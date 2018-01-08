2bdd9282f01577f591dee5c94b263abe30d1a6ec
stanley.joseph <stanley.joseph@E13-RDS11-Microservice.newvoe.local>
2018-01-08 11:27:13 +0000

    Initiated Spring Boot Project

---
+++ dal-broadband-svc/src/main/java/com/vf/uk/dal/broadband/DalBroadbandSvcApplication.java
@@ -0,0 +1,12 @@
+package com.vf.uk.dal.broadband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalBroadbandSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalBroadbandSvcApplication.class, args);
+	}
+}
