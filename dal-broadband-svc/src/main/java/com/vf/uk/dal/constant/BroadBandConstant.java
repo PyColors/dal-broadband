f4855ab1ca9bc3c3a0f0cbdf0cf0bed468dffb12
aditya.soni01 <aditya.soni01@E13-RDS14-P11.newvoe.local>
2018-01-15 14:38:24 +0000

    Changes to the response

---
+++ dal-broadband-svc/src/main/java/com/vf/uk/dal/constant/BroadBandConstant.java
@@ -0,0 +1,8 @@
+package com.vf.uk.dal.constant;
+
+public class BroadBandConstant {
+
+	public static final String LINE_WITH_76 = "Line and Fibre with Speed 76";
+	public static final String LINE_WITH_38 = "Line and Fibre with Speed 38";
+
+}
