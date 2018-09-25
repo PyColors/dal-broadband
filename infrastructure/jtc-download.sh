b00d9b3ae198c2f779551bcd9fe9d32f927e4c05
Ruaridh Angus <ruaridh.angus1@vodafone.com>
2018-09-25 07:50:58 +0000

    Deleted infrastructure

--- infrastructure/jtc-download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo ${jtclocation}/${jtcfilename}
-aws s3 cp s3://${jtclocation}/${jtcfilename} .
-tar -C /apps/jtc-config -xf ./${jtcfilename}
-rm -f ./${jtcfilename}
-
-java ${JAVA_OPTS} -jar /apps/sb/${MSNAME}-svc-${VERSION}.jar
