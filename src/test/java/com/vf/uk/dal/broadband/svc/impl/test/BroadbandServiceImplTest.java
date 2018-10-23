8783b0d7cdfc9452c38a8698fe802d53525e3805
jason.hart <jason.hart@ENGLON217791L.ad.infosys.com>
2018-10-23 17:20:18 +0100

    Added service Implementation Tests and updated pom

--- src/test/java/com/vf/uk/dal/broadband/svc/impl/test/BroadbandServiceImplTest.java
+++ src/test/java/com/vf/uk/dal/broadband/svc/impl/test/BroadbandServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.vf.uk.dal.broadband.svc.impl.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,19 +17,36 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vf.uk.dal.broadband.basket.entity.Basket;
+import com.vf.uk.dal.broadband.basket.entity.BasketRequest;
+import com.vf.uk.dal.broadband.basket.entity.CreateBasketRequest;
 import com.vf.uk.dal.broadband.beans.test.BroadbandTestBeans;
 import com.vf.uk.dal.broadband.cache.repository.entity.Broadband;
 import com.vf.uk.dal.broadband.dao.BroadbandDao;
 import com.vf.uk.dal.broadband.entity.AvailabilityCheckRequest;
 import com.vf.uk.dal.broadband.entity.AvailabilityCheckResponse;
+import com.vf.uk.dal.broadband.entity.BundleDetails;
+import com.vf.uk.dal.broadband.entity.CreateAppointmentResponse;
 import com.vf.uk.dal.broadband.entity.EnhanceCompatibleExtraResponse;
+import com.vf.uk.dal.broadband.entity.FlbBundle;
+import com.vf.uk.dal.broadband.entity.GetAppointmentResponse;
+import com.vf.uk.dal.broadband.entity.GetBundleListSearchCriteria;
+import com.vf.uk.dal.broadband.entity.OptimizePackageRequest;
+import com.vf.uk.dal.broadband.entity.OptimizePackageResponse;
 import com.vf.uk.dal.broadband.entity.RouterDetails;
 import com.vf.uk.dal.broadband.entity.RouterProductDetails;
 import com.vf.uk.dal.broadband.entity.SelectedAvailabilityCheckResponse;
 import com.vf.uk.dal.broadband.entity.ServiceStartDateRequest;
+import com.vf.uk.dal.broadband.entity.appointment.CreateAppointment;
+import com.vf.uk.dal.broadband.entity.appointment.GetAppointment;
+import com.vf.uk.dal.broadband.entity.customer.Account;
+import com.vf.uk.dal.broadband.entity.premise.AddressInfo;
 import com.vf.uk.dal.broadband.entity.product.CommercialProduct;
+import com.vf.uk.dal.broadband.entity.promotion.BundlePromotion;
+import com.vf.uk.dal.broadband.journey.entity.CurrentJourney;
 import com.vf.uk.dal.broadband.svc.impl.BroadbandServiceImpl;
 import com.vf.uk.dal.broadband.tests.util.FileUtility;
+import com.vf.uk.dal.broadband.utils.BroadbandRepoProvider;
 import com.vf.uk.dal.entity.serviceavailability.GetServiceAvailibilityResponse;
 
 @RunWith(SpringRunner.class)
@@ -41,9 +59,42 @@ public class BroadbandServiceImplTest {
 
 	@MockBean
 	BroadbandDao broadbandDao;
+	
+	@MockBean
+	BroadbandRepoProvider broadBandRepoProvider;
 
 	@Before
 	public void setupMockBehaviour() throws Exception {
+		
+		String getBroadbandCacheResponse = new String(FileUtility.readFile("\\rest-mock\\BroadbandCacheResponse.json"));
+		Broadband broadbandCacheResponse = new ObjectMapper().readValue(getBroadbandCacheResponse, Broadband.class);
+		Mockito.when(broadbandDao.getBroadbandFromCache(Mockito.anyString())).thenReturn(broadbandCacheResponse);
+
+		
+		String getCurrentJourneyResponse = new String(FileUtility.readFile("\\rest-mock\\GetCurrentJourneyResponse.json"));
+		CurrentJourney currentJourney = new ObjectMapper().readValue(getCurrentJourneyResponse, CurrentJourney.class);
+		Mockito.when(broadbandDao.getJourney(Mockito.anyString())).thenReturn(currentJourney);
+		
+		String getBasketJsonResponse = new String(FileUtility.readFile("\\rest-mock\\GetBasket.json"));
+		Basket basketResponse = new ObjectMapper().readValue(getBasketJsonResponse, Basket.class);
+		Mockito.when(broadbandDao.getBasket(Mockito.anyString())).thenReturn(basketResponse);
+		
+		String createBasketJsonRequest = new String(FileUtility.readFile("\\rest-mock\\CreateBasketRequest.json"));
+		CreateBasketRequest createBasketRequest = new ObjectMapper().readValue(createBasketJsonRequest,
+				CreateBasketRequest.class);
+		Mockito.when(broadbandDao.createBasket(createBasketRequest)).thenReturn(basketResponse);
+		
+//		String createApointmentRequest = new String(FileUtility.readFile("\\rest-mock\\CreateAppointmentRequest.json"));
+//		com.vf.uk.dal.broadband.entity.appointment.CreateAppointmentRequest createApptRequest = new ObjectMapper()
+//				.readValue(createApointmentRequest,
+//						com.vf.uk.dal.broadband.entity.appointment.CreateAppointmentRequest.class);
+		
+		String createAppointmentJsonResponse = new String(
+				FileUtility.readFile("\\rest-mock\\CreateAppointment_Response.json"));
+		CreateAppointment createApptResponse = new ObjectMapper().readValue(createAppointmentJsonResponse,
+				CreateAppointment.class);
+		
+		Mockito.when(broadbandDao.createAppointment(Mockito.any(com.vf.uk.dal.broadband.entity.appointment.CreateAppointmentRequest.class))).thenReturn(createApptResponse);
 	}
 
 	@Test
@@ -115,4 +166,108 @@ public class BroadbandServiceImplTest {
 		Mockito.when(broadbandDao.getBroadbandFromCache(Mockito.anyString())).thenReturn(broadbandCacheResponse);
 		serviceImpl.clearPackageAndBasketCache("12345678907888", "123456789078");
 	}
+	
+	@Test
+	public void testGetFlbList() throws Exception {
+		String getBundleListSearchCriteriaString = new String(FileUtility.readFile("\\rest-mock\\getBundleListSearchCriteriaRequest.json"));
+		GetBundleListSearchCriteria getBundleListSearchCriteria = new ObjectMapper().readValue(getBundleListSearchCriteriaString,
+				GetBundleListSearchCriteria.class); 
+		
+		String bundleDetailsResponseString = new String(FileUtility.readFile("\\rest-mock\\GetFLBBListResponse.json"));
+		BundleDetails bundleDetailsResponse = new ObjectMapper().readValue(bundleDetailsResponseString,
+				BundleDetails.class);
+		
+		Mockito.when(broadbandDao.getBundleDetailsFromGetBundleListAPI(Mockito.anyString())).thenReturn(bundleDetailsResponse);
+
+		List<FlbBundle> flbBundleList = serviceImpl.getFlbList(getBundleListSearchCriteria);
+		
+		assertEquals( "Home Broadband - Unlimited Fibre 38 and Phone - FTR4 -18 m" , flbBundleList.get(0).getBundleName());
+		assertEquals( "Line and ADSL" , flbBundleList.get(0).getClassificationCode());
+		assertEquals( true, flbBundleList.get(0).getIsSelected());
+		
+	}
+	
+	@Test
+	public void testGetAddressInfoByPostcodeFromPremise() throws Exception {
+		String getAddressListJsonResponse = new String(
+				FileUtility.readFile("\\rest-mock\\GetAddressByPostCode_Response.json"));
+		AddressInfo galResponse = new ObjectMapper().readValue(getAddressListJsonResponse, AddressInfo.class);
+		Mockito.when(broadbandDao.getAddressInfoByPostcodeFromPremise(Mockito.any(), Mockito.any())).thenReturn(galResponse);
+		AddressInfo addressInfo = serviceImpl.getAddressInfoByPostcodeFromPremise("LS290JJ", "FTTH", "Consumer");
+		
+		assertEquals( "LS29 0JJ" , addressInfo.getAddresses().get(0).getPostCode());
+		assertEquals( "A90000221048" , addressInfo.getAddresses().get(0).getId());
+		assertEquals( "Gold" , addressInfo.getAddresses().get(0).getContextId());	
+		
+	}
+	
+	@Test
+	public void testCreateOrUpdatePackage() throws Exception {
+		String BasketJsonRequest = new String(FileUtility.readFile("\\rest-mock\\BasketRequest.json"));
+		BasketRequest basketRequest = new ObjectMapper().readValue(BasketJsonRequest,
+				BasketRequest.class);
+		
+		String broadbandJson = new String(FileUtility.readFile("\\rest-mock\\Broadband.json"));
+		Broadband broadband = new ObjectMapper().readValue(broadbandJson,
+				Broadband.class);
+		
+		Basket basket = serviceImpl.createOrUpdatePackage(basketRequest, broadband, "123456789078");
+
+		assertEquals( "2017-10-01T09:45:00.000+00:00" , basket.getCustomerRequestedDate());
+		assertEquals( "11" , basket.getAffiliateId());
+		assertEquals( "25" , basket.getPriceDetails().getMonthlyPrice().getGross());
+	}
+	
+	@Test
+	public void testCreateAppointment() throws Exception {
+		
+		String createApointmentRequest = new String(FileUtility.readFile("\\rest-mock\\CreateAppointmentRequest2.json"));
+		com.vf.uk.dal.broadband.entity.CreateAppointmentRequest createApptRequest = new ObjectMapper()
+				.readValue(createApointmentRequest,
+						com.vf.uk.dal.broadband.entity.CreateAppointmentRequest.class);
+		
+		CreateAppointmentResponse response = serviceImpl.createAppointment(createApptRequest, "12345678907888");
+		
+		assertEquals("string", response.getApplicationId());
+		
+	}
+	
+	@Test
+	public void testGetAppointmentForFLBB() throws Exception {
+
+		String jsonStringGAResponse = new String(FileUtility.readFile("\\rest-mock\\GetAppointment_Response.json"));
+		GetAppointment getAppointmentResponse = new ObjectMapper().readValue(jsonStringGAResponse,
+				GetAppointment.class);
+		
+		Mockito.when(broadbandDao.getAppointmentList(Mockito.any(com.vf.uk.dal.broadband.entity.appointment.GetAppointmentRequest.class))).thenReturn(getAppointmentResponse);
+
+		GetAppointmentResponse response = serviceImpl.getAppointmentForFLBB("12345678907888");
+		
+		assertEquals("2018-05-22 11:00:00", response.getAppointmentWindowList().get(0).getStartDate());
+		assertEquals("2018-05-24 11:00:00", response.getAppointmentWindowList().get(4).getStartDate());
+		assertEquals("2018-05-29 11:00:00", response.getAppointmentWindowList().get(9).getStartDate());
+		
+	}
+	
+	@Test
+	public void testOptimizePackageForFLBB() throws Exception {
+		String getAccountResponseUsingAccountIdJson = new String(
+				FileUtility.readFile("\\rest-mock\\GetCustomerUsingAccountId.json"));
+		Account[] getAccountResponseUsingAccountId = new ObjectMapper().readValue(getAccountResponseUsingAccountIdJson,
+				Account[].class);
+		Mockito.when(broadbandDao.getAccountDetailsByAccountId(Mockito.anyString())).thenReturn(Arrays.asList(getAccountResponseUsingAccountId));
+		
+		String promotionResponse = new String(FileUtility.readFile("\\rest-mock\\GetPromotionForBundle.json"));
+		BundlePromotion[] bundlePromotion = new ObjectMapper().readValue(promotionResponse, BundlePromotion[].class);
+		Mockito.when(broadbandDao.getPromotionForBundleList(Mockito.any(com.vf.uk.dal.broadband.entity.promotion.BundlePromotionRequest.class))).thenReturn(Arrays.asList(bundlePromotion));
+	
+		OptimizePackageRequest optimizePackageRequest = new OptimizePackageRequest();
+		optimizePackageRequest.setJourneyId("2290f3b1-1ed5-4513-9f86-110531c5fbfb");
+		
+		OptimizePackageResponse response = serviceImpl.optimizePackageForFLBB(optimizePackageRequest, "12345678907888");
+		
+		assertEquals(true, response.getHasPackageOptimized());
+		
+	}
+	
 }
